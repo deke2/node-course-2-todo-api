3778a67ed9a4707fae63bfef0fc00cd337a0327a
Deke <>
2018-12-31 19:37:04 -0500

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -61,21 +61,44 @@ UserSchema.statics.findByToken = function (token) {
   } catch (e) {
     return Promise.reject();
   }
+
   return User.findOne({
     '_id': decoded._id,
     'tokens.token': token,
     'tokens.access': 'auth'
   });
 };
-//; ****************************
+
+//********************** */
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      // Use bcrypt.compare to compare password and user.password
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+  });
+};
+
 UserSchema.pre('save', function (next) {
   var user = this;
 
   if (user.isModified('password')) {
     bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(user.password, salt, (err, hash) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
         user.password = hash;
-        next();  
+        next();
       });
     });
   } else {
