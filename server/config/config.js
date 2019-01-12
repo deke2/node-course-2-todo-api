9e749e982b8e3ad5bf9a8a49939040cfd6d6ab54
Deke <>
2019-01-12 13:13:28 -0500

    Swiched from expect version 1 to 21

--- server/config/config.js
+++ server/config/config.js
@@ -9,11 +9,3 @@ if (env === 'development' || env === 'test') {
       process.env[key] = envConfig[key];
     });
 }
-
-// if (env === 'development') {
-//     process.env.PORT = 3000;
-//     process.env.MONGODB_URI = 'mongodb://localhost:27017/ToDoApp';
-// } else if (env === 'test') {
-//     process.env.PORT = 3000;
-//     process.env.MONGODB_URI = 'mongodb://localhost:27017/ToDoAppTest';
-// }
